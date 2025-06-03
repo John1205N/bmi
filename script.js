a4dcf27b37644044bd4361e802cc0bba28ab41b5
johne <john12>
2025-06-03 17:12:53 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+function calculateBMI(event){
+    event.preventDefault(); // prevent form submission
+    
+    const height = parseFloat(document.getElementById("height").value);
+    const weight = parseFloat(document.getElementById("weight").value);
+
+    if (height > 0 && weight > 0) {
+        const bmi = weight / (height * height);
+        let category = "";
+
+        if (bmi < 18.5) {
+            category = "Underweight";
+        } else if (bmi < 24.9) {
+            category = "Normal weight";
+        } else if (bmi < 29.9) {
+            category = "Overweight";
+        } else {
+            category = "Obese";
+        }
+        document.getElementById("result").innerText = `Your  Bmi Is ${bmi.toFixed(2)} ${category}.`;
+    }
+    else {
+
+        document.getElementById("result").innerText =
+          "Please enter valid height and weight.";
+      }
+    }
